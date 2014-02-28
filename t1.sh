54483a72df6af9ee88684e521109fee7630dbb1c
root <root@localhost.localdomain>
2014-02-28 15:09:19 +0100

    add more code.

--- t1.sh
+++ t1.sh
@@ -4,6 +4,8 @@ testEquality()
 {
   assertEquals 1 1
 }
+echo "current dir:$PWD\n"
+echo "this bash pid:$$\n"
 
 testPartyLikeItIs1999()
 {
@@ -11,7 +13,13 @@ testPartyLikeItIs1999()
   assertEquals "It's not 1999 :-( This is ${year}." \
       '1999' "${year}"
 }
-
+cd /root/vertxworld
+echo "current dir: $PWD\n"
+/usr/local/bin/vertx runmod com.m3958.vertxio~vertxcombo~0.0.1.8 1>combo.log 2>&1 &
+echo "oldpwd is:$OLDPWD"
+echo "last executed pid:$!"
+echo $! >/root/vertxworld/pid
+cd $OLDPWD
 # load shunit2
 . ./shunit2
 
